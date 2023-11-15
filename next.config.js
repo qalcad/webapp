8cbc172e9ee010b42625b66478490cebd9f30a66
Ali Moallim <axj.159@gmail.com>
2023-11-15 06:59:18 +0300

    Update next.config.js

--- next.config.js
+++ next.config.js
@@ -3,7 +3,8 @@ const isProd = process.env.NODE_ENV === "production";
 module.exports = {
   output: "export",
   images: { unoptimized: true },
-  assetPrefix: "./webapp",
+  basePath: "/webapp",
+  assetPrefix: "webapp",
   env: {
     googleAPIKey: process.env.GOOGLE_API_KEY
   }
